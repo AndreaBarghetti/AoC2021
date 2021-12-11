175a0e872977d19be4c8bb24904c52f5a031d8d3
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2021-12-11 22:31:19 +0100

    vis

--- Day1/day1.R
+++ Day1/day1.R
@@ -15,14 +15,16 @@ seafloor <- ggplot(data=NULL, aes(x=seq_along(input), y=max(input)-input)) +
   geom_segment(aes(y=1:max(input),
                    yend=1:max(input),
                    x=1,
-                   xend=length(input),
-                   col=1:max(input), alpha=.1)) +
-  geom_area() +
-  theme_void()
+                   xend=length(inv_input),
+                   col=1:max(inv_input), alpha=.1),
+               show.legend = F) +
+  geom_area(show.legend = F) +
+  theme_void() +
+  theme(legend.position = "none")
 
 ggsave(plot = seafloor, 
        filename = "seafloor.png", 
        device = "png", 
        path = "Day1/",
-       width = 10,
-       height = 10, units = "cm")
+       width = 5,
+       height = 5, units = "cm")
