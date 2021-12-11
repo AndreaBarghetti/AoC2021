837d69b4dae1970c0c3805d0eeaa08f28a5f326c
Andrea Barghetti <32954484+AndreaBarghetti@users.noreply.github.com>
2021-12-11 20:23:35 +0100

    animation9

--- Day9/day9.R
+++ Day9/day9.R
@@ -28,34 +28,21 @@ sum(space[find_low_points(space)]+1)
 space2 <- ifelse(space==9,0,1)
 space2[which(find_low_points(space))] <- which(find_low_points(space))
 
-# how does it look like?
-# edges in black, low points in red
-space2 %>% 
-  as_tibble() %>% 
-  rownames_to_column("row") %>% 
-  gather(col, value, contains("V")) %>% 
-  mutate(col=str_remove(col,"V") %>% as.integer(),
-         val = ifelse(value<=1,value,2)) %>% 
-  ggplot(aes(x=row,y=col)) +
-  geom_tile(aes(fill=as_factor(val)),show.legend=F) +
-  scale_fill_manual(values = c("black","white","red")) +
-  theme_void()
-
 # spread the values of low points to surroundings by max values
 # without touching the edges (the 0s)
 # and repeat until it's stable
 spread_max <- function(space2) {
-  space2 <- apply(space2, 1, function(x) {
+  x <- apply(space2, 1, function(x) {
     r <- pmap_dbl(list(lag(x,default =0), x, lead(x,default = 0)), max)
     r[x==0]<-0
     r
   }) %>% t()
-  space2 <- apply(space2, 2, function(x) {
+  y <- apply(space2, 2, function(x) {
     r <- pmap_dbl(list(lag(x,default =0), x, lead(x,default = 0)), max)
     r[x==0]<-0
     r
   })
-  space2
+  map2_dbl(x,y, max) %>% matrix(ncol=ncol(space2))
 }
 
 spread_max_all <- function(space2) {
@@ -73,3 +60,37 @@ maxed_space[!maxed_space%in%0:1] %>%
   head(3) %>% 
   prod()
 
+# animation ####
+library(reshape2)
+
+lowp_num <- sum(space2>1)
+cols = rainbow(lowp_num, s=.6, v=.9)[sample(1:lowp_num,lowp_num)]
+set_levels <- unique(as_factor(space2))
+
+plot_space <- function(space) {
+  space <- melt(space) %>% 
+    mutate(value=factor(value, levels = set_levels))
+  space %>%
+    ggplot(aes(x=Var1,y=Var2)) +
+    geom_tile(aes(fill=value),show.legend=F) +
+    scale_fill_manual(values = c("white","black",cols), drop=FALSE) +
+    theme_void()
+}
+
+film_basins <- function(space2,filename) {
+  saveGIF({
+    while(!all(space2==spread_max(space2))) {
+      print(plot_space(space2))
+      space2 <- spread_max(space2)
+    }
+    print(plot_space(space2))
+  },
+  loop=T,
+  interval=0.5,
+  ani.width = 500, 
+  ani.height = 500,
+  movie.name = filename, 
+  outdir = getwd()
+  )
+}
+film_basins(space2, "basins.gif")
